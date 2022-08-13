9efd46f38442283d5a527cb29b5798ead1343737
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-13 23:46:33 +0300

    task 2 without throttle work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+const onTimeUpdate = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', _.throttle(onTimeUpdate, 1000));
+console.log(localStorage);
+
+const stopTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(stopTime);
